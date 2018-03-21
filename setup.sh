c905eb2617b0576b6f2dde07ec6f518ce0486b5a
devurbandictionary <32337925+devurbandictionary@users.noreply.github.com>
2018-03-20 17:56:35 -0700

    initial commit - run the scripts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir data
+
+wget -c http://mattmahoney.net/dc/enwik9.zip -P data
+unzip data/enwik9.zip -d data
+
+perl fastText/wikifil.pl data/enwik9 > data/fil9
+
+mkdir result
+
+./fastText/fasttext skipgram -input data/fil9 -output result/fil9
